a25374a5cb27624cac669d7808a715e4a7f1d41e
Natalia Sorokina <pticasonabird@gmail.com>
2020-12-14 18:24:09 +0300

    выполнено задание 5

---
+++ js/task-5.js
@@ -0,0 +1,25 @@
+const products = [
+  { name: 'Радар', price: 1300, quantity: 4 },
+  { name: 'Сканер', price: 2700, quantity: 3 },
+  { name: 'Дроид', price: 400, quantity: 7 },
+  { name: 'Захват', price: 1200, quantity: 2 },
+];
+
+const getAllPropValues = function(arr, prop) {
+    const propValues = [];
+    for (const product of products) {
+        if (product[prop] !== undefined) {
+            propValues.push(product[prop]);
+        }
+    }
+    return propValues;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности реализации.
+ */
+console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+
+console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
+
+console.log(getAllPropValues(products, 'category')); // []
